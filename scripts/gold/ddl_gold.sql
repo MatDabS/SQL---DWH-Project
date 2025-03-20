641d279b2285da7542af36ea291e2c61ac45d75f
Mateusz Dąbrowski <matdabrowski98@gmail.com>
2025-03-20 18:03:38 +0100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,18 +1,29 @@
+/*
+======================================================
+DDL Script: Create Bronze Tables
+======================================================
+Script purpose:
+		This script creates tables in the 'bronze' schema, dropping existing tables
+		if they already exist.
+		Run this script to re-define the DDL structure of 'bronze' tables.
+======================================================
+*/
+
 CREATE VIEW gold.dim_customers AS 
 SELECT
-ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
-ci.cst_id AS customer_id,
-ci.cst_key AS customer_number,
-ci.cst_firstname AS first_name,
-ci.cst_lastname AS last_name,
-la.cntry AS country,
-ci.cst_marital_status AS marital_status,
-CASE
-	WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
-	ELSE COALESCE(ca.gen, 'n/a')
-END AS gender,
-ca.bdate AS birthdate,
-ci.cst_create_date AS create_date
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
 FROM silver.crm_cust_info AS ci
 LEFT JOIN silver.erp_cust_az12 AS ca
 ON		    ci.cst_key = ca.cid
